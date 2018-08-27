3fd1d01c39e156af32bea5470aed7fefd376bf08
Leandro Schuab <lschuab@gmail.com>
2018-08-26 21:59:12 -0700

    added album search using lastFM API

--- scripts/bin.js
+++ scripts/bin.js
@@ -1,14 +1,33 @@
 const albumScroll = document.getElementById('album-scroll');
 const bin = document.getElementById('bin');
+const apiKey = '27c16b31f7afa331a59fd0bed30d96e1';
 
-axios.get('https://lit-fortress-6467.herokuapp.com/object')
+// axios.get('https://lit-fortress-6467.herokuapp.com/object')
+//   .then(response => {
+//       const data = response.data.results;
+//       for (const album of data) {
+//         albumScroll.insertAdjacentHTML('beforeend', `
+//           <img src="images/${album.cover_art}" alt="" class="add-album" id="${album.artist}: ${album.title}">
+//         `);
+//       }
+// });
+
+
+document.getElementById('search-btn').addEventListener('click', e => {
+  e.preventDefault();
+  albumScroll.textContent = '';
+  const searchTerms = document.getElementById('search-input').value;
+  axios.get(`http://ws.audioscrobbler.com/2.0/?method=album.search&album=${searchTerms}&api_key=${apiKey}&format=json&limit=20`)
   .then(response => {
-      const data = response.data.results;
-      for (const album of data) {
-        albumScroll.insertAdjacentHTML('beforeend', `
-          <img src="images/${album.cover_art}" alt="" class="add-album" id="${album.artist}: ${album.title}">
-        `);
-      }
+    const data = response.data.results.albummatches.album;
+    for (const album of data) {
+      albumScroll.insertAdjacentHTML('beforeend', `
+        <img src="${album.image[3]['#text']}" alt="" class="add-album" id="${album.artist}: ${album.name}">
+      `);
+    }
+
+
+  });
 });
 
 
--- scripts/splash.js
+++ scripts/splash.js
@@ -1,13 +1,39 @@
-axios.get('https://lit-fortress-6467.herokuapp.com/object')
-  .then(response => {
-      const data = response.data.results;
-      const selectedPics = [];
-      let picIndex = 0;
-      for (let i = 1; i <= 3; i++) {
-        do {
-          picIndex = Math.floor(Math.random() * data.length);
-        } while (selectedPics.includes(picIndex));
-        selectedPics.push(picIndex);
-        document.getElementById(`album${i}`).src = `images/${data[picIndex].cover_art}`;
-      }
+// Implementation with original API
+// axios.get('https://lit-fortress-6467.herokuapp.com/object')
+//   .then(response => {
+//       const data = response.data.results;
+//       const selectedPics = [];
+//       let picIndex = 0;
+//       for (let i = 1; i <= 3; i++) {
+//         do {
+//           picIndex = Math.floor(Math.random() * data.length);
+//         } while (selectedPics.includes(picIndex));
+//         selectedPics.push(picIndex);
+//         document.getElementById(`album${i}`).src = `images/${data[picIndex].cover_art}`;
+//       }
+// });
+
+// Uses lastFM API to get current top 50 artists, chooses three f those at radom, then uses the api again to get the image of the top album of each of those three artists
+const apiKey = '27c16b31f7afa331a59fd0bed30d96e1';
+const albums = [];
+
+axios.get(`http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${apiKey}&format=json`)
+.then(response => {
+  const promiseArr = [];
+  const topArtists = response.data.artists.artist;
+  const selectedArtists = [];
+  let artistIndex = 0;
+  for (let i = 1; i <= 3; i++) {
+    do {
+      artistIndex = Math.floor(Math.random() * topArtists.length);
+    } while (selectedArtists.includes(artistIndex));
+    selectedArtists.push(artistIndex);
+    promiseArr.push(axios.get(`http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=${topArtists[artistIndex].name}&api_key=${apiKey}&format=json&limit=1`));
+  }
+  return Promise.all(promiseArr);
+})
+.then(promises => {
+  for (let i = 0; i< 3; i++ ) {
+      document.getElementById(`album${i + 1}`).src = promises[i].data.topalbums.album[0].image[3]['#text'];
+  }
 });
